ffd9e3323a32e5c9d7a4a588f45041194cf329cb
ksmiti <kilian.smiti@outlook.fr>
2020-09-26 19:46:26 +0200

    +remove some useless methods

--- main.go
+++ main.go
@@ -147,3 +147,30 @@ func execute(dir *string, name string, args... string) {
 		}
 	}
 }
+
+const (
+	InfoColor    = "\033[1;34m"
+	WarningColor = "\033[1;33m"
+	ErrorColor   = "\033[1;31m"
+	SuccessColor = "\033[1;32m"
+	ResetColor   = "\033[0;0m"
+)
+
+func logger (msg string, args... interface{}) {
+	fmt.Println(">", fmt.Sprintf(msg, args...))
+}
+
+func _error (msg string, args... interface{}) {
+	fmt.Println(ErrorColor, "Fatal Error > ", fmt.Sprintf(msg, args...), ResetColor)
+	os.Exit(0)
+}
+
+func loggerCount (i int, count int) {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	fmt.Println(WarningColor, "=============================")
+	fmt.Println(WarningColor, "    🚀 NPM Booster 1.0.0    ")
+	fmt.Println(WarningColor, "=============================")
+	fmt.Printf("%sWe have actually build %d / %d package(s)%s\n", SuccessColor, i, count, ResetColor)
+}
